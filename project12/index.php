138f676f5b6a283d0eaa97c60132a5a4dd8ce777
vanhieu0273 <2151060261@e.tlu.edu.vn>
2024-02-20 20:21:28 +0700

    project

---
+++ project12/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Project1</title>
+</head>
+<body>
+
+<?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+
+$firstItemIcon = "fas fa-home";
+?>
+
+<nav>
+    <ul>
+        <li><i class="<?php echo $firstItemIcon; ?>"></i><a href="#"><?php echo $navItems[0]; ?></a></li>
+        <?php foreach (array_slice($navItems, 1) as $item): ?>
+            <li><a href="#"><?php echo $item; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
+</body>
+</html>
